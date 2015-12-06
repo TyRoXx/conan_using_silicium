47310125c53d80e4ff09af6616955ccb2d9e3bc8
TyRoXx <tyroxxxx@gmail.com>
2015-12-06 01:07:15 +0100

    Boost should be referenced automatically by silicium

--- conanfile.py
+++ conanfile.py
@@ -2,6 +2,6 @@ from conans import ConanFile, CMake
 
 class ConanUsingSilicium(ConanFile):
 	settings = "os", "compiler", "build_type", "arch"
-	requires = "Boost/1.59.0@lasote/stable", "silicium/0.1@tyroxx/testing"
+	requires = "silicium/0.1@tyroxx/testing"
 	generators = "cmake"
 	default_options = "Boost:shared=True"
